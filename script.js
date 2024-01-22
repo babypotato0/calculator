20d81dfabe879c8849ba972f2d47c6914b4bc119
Monica <monicahutama@gmail.com>
2024-01-22 13:51:34 +0700

    Created the operate function. Functionality not yet added

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+   return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+ };
+
+function multiply(a, b) {
+    return a * b;
+ };
+
+function divide(a, b) {
+    return a / b;
+};
+
+let numOne;
+let operator;
+let numTwo;
+
+function operate(numOne, operator, numTwo) {
+    multiply(numOne, numTwo);
+}
